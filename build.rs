87e3e88048d67905ea0cb79ad3948cc7f945c404
Kaushik Kulkarni <kaushikcfd@gmail.com>
2022-09-28 17:36:14 -0500

    able to link to libisl

--- build.rs
+++ build.rs
@@ -6,8 +6,10 @@ use std::path::Path;
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     let num_jobs = env::var("NUM_JOBS").unwrap();
+    let isl_dir_path = Path::new("isl/");
+    let pwd_at_build_start_path = env::current_dir().unwrap();
 
-    if !Path::new("isl/").is_dir() {
+    if !isl_dir_path.is_dir() {
         panic!("isl directory not found. Most likely you missed calling `git submdoule update --init`.");
     }
 
@@ -15,23 +17,31 @@ fn main() {
         panic!("isl directory not found. Most likely you missed calling `git submdoule update --init --recursive`.");
     }
 
-    Command::new("isl/autogen.sh")
+    // Got to isl/ before building anything
+    if !env::set_current_dir(&isl_dir_path).is_ok() {
+        panic!("Could not cd into isl/")
+    }
+
+    Command::new("./autogen.sh")
         .status()
         .expect("failed to autoreconf!");
-    Command::new("isl/configure")
+    Command::new("./configure")
         .args(["--prefix", out_dir.as_str()])
         .status()
         .expect("failed to configure!");
     Command::new("make")
-        .args(&[
-              "-j", num_jobs.as_str()])
+        .args(&["-j", num_jobs.as_str()])
         .status()
         .expect("failed to make!");
     Command::new("make")
-        .args(&[
-              "install"])
+        .args(&["install"])
         .status()
         .expect("failed to make install!");
+
+    // Got to out_dir/ before building anything
+    if !env::set_current_dir(pwd_at_build_start_path).is_ok() {
+        panic!("Could not cd back into OUT_DIR")
+    }
 
     let isl_lib_dir = Path::new(out_dir.as_str()).join("lib/");
 
